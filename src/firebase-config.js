99f0d3ecc620b320a25b0d5e1782bbc771c7f014
Krystsina Kavalevich <krikoax@gmail.com>
2023-01-02 12:45:53 +0100

    feat: add firebase to the project

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqJq251LxnMZVwBEZxcviU7Kluk9rRkXk",
+  authDomain: "to-do-47c88.firebaseapp.com",
+  projectId: "to-do-47c88",
+  storageBucket: "to-do-47c88.appspot.com",
+  messagingSenderId: "483597153569",
+  appId: "1:483597153569:web:7569f0a94981e39517ba60",
+};
+
+export const app = initializeApp(firebaseConfig);
